f077646aac80ef64f880e9939a68ae93f1f6bf9a
biancamdros <bibilaise@gmail.com>
2022-11-19 15:52:49 -0300

    tarefa5

---
+++ Next0/pages/tarefa-05/exe-02/movieDetails/[movieID].js
@@ -0,0 +1,5 @@
+import MoviesDetails from "../../../tarefa-04/exe-01/movieDetails/[movieID]"
+
+export default function Exercicio2 (){
+    return <MoviesDetails />
+}
---
+++ Next0/pages/tarefa-05/exe-03/movie.js
@@ -0,0 +1,98 @@
+import useSWR from 'swr'
+import {useState} from 'react'
+import Link from 'next/link'
+
+export default function Movies3(){
+
+    const [url, setUrl] = useState('')
+    const {data, error} = useSWR(url, theFetcher)
+
+    const onClickHandler = (e) => {
+        const link = e.target.id
+
+        e.preventDefault()
+        if (url === '') setUrl(link)
+
+        else setUrl('')
+
+    }
+
+    return (
+
+        <div>
+
+            <TheLink url={url} link={'https://www.omdbapi.com/?apikey=a85a78d&s=bagdad'} handler={onClickHandler}/>
+            <TheLink url={url} link={'https://www.omdbapi.com/?apikey=a85a78d&s=2012'} handler={onClickHandler} />
+            <TheMovies data={ error?{error:'Erro na pesquisa'}: data ? data: {Search:''} } show={url !== ''}/>
+        </div>
+
+    )
+
+}
+
+async function theFetcher(url) {
+
+    if (url === null || url === '') return {Search:''}
+    const res = await fetch(url);
+    const json = await res.json();
+    return json;
+
+}
+
+
+
+export function TheMovies({data,show}){
+
+    
+
+    if (!show) return (<div></div>)    
+
+    if (data.error) return (<div>falha na requisição</div>)
+
+    if (data.Search === '' ) return (<div>carregando...</div>)
+
+
+        
+
+    return (
+
+        <div>
+            { data.Search.map( (m, i) => 
+                
+                <div key={i}>
+                    <Link href={'/tarefa-05/exe-03/movies'} id={m.imdbID} onClick={loadingMovie}>{m.Title}</Link>
+                </div>  
+            ) }            
+
+        </div>
+
+    )
+
+}
+
+
+
+
+function loadingMovie(e) {
+
+    e.preventDefault()
+    const {data, error} = useSWR(`http://www.omdbapi.com/?apikey=a85a78d&i=${e.target.id}`, theFetcher)
+    console.log(data)
+  
+  }
+  
+
+
+export function TheLink({url, handler, link}){    
+
+    return (
+
+        <div>
+
+            <a href="/tarefa-05/exe-01/movies.js" id={link} onClick={handler}> {url === '' ? 'Mostrar' : 'Ocultar'} </a>
+
+        </div>
+
+    )
+
+}
